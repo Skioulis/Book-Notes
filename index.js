0417ff70f38d220e98628619eaefab508261dbb8
Fotis Fotiadis <skioulis@gmail.com>
2025-06-19 12:17:48 +0300

    add basic project structure including views, static assets, and server setup

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('Happy developing ✨')
+import express from 'express';
+import bodyParser from 'body-parser';
+import pg from 'pg';
+
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "book-notes",
+    password: "123456",
+    port: 5432,
+});
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get('/', async (req, res) => {
+    res.render('index.ejs');
+} )
+
+app.listen(port, () => {
+    console.log(`Server running @ http://localhost:${port}`);
+});
